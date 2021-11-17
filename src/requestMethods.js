d92836166b9e08edffe1d617673f7e7d122e4a59
dwarez12 <85868487+dwarez12@users.noreply.github.com>
2021-11-17 17:45:11 +0400

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.PORT;
+const BASE_URL = "pharma2021-backend.herokuapp.com/api";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxN2VhNjMwNGNlNzJmMThmOTU4MjcyYiIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzU3MDIxNTIsImV4cCI6MTYzNTk2MTM1Mn0.PXSA_28BvGH2bFSQv3o1Q3SZ5qKDd57sc7x5fr8PbBk";
 
 export const publicRequest = axios.create({
